97f2815b7c43430683d9830ba69c18ee17e00d79
erfan <erfandc.es@gmail.com>
2025-11-29 00:13:54 +0330

    fix layout bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen">
         <Navbar/>
         <ThemeUpdater/>
-        {children}
+        <main className="flex-1">
+          {children}
+        </main>
         <Footer/>
       </body>
     </html>
